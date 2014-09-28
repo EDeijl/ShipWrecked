efe0eac08d8c1ea18fed46b5cb8d1a37c96f41b3
Erik Deijl <erik.deijl@gmail.com>
2014-09-28 15:19:24 +0200

    added walking buttons to gui, and event handlers for touch and click events

--- hud.lua
+++ hud.lua
@@ -11,17 +11,17 @@ function HUD:initialize ()
   -- and be more window based, we create 
   -- a new viewport for it.
   self.viewport = MOAIViewport.new ()
-  
+
   -- To make this viewport more intuitive,
   -- we'll put the (0,0) in the top left corner
   -- and make the y axis grow to the bottom
-  
+
   viewport:setSize ( SCREEN_RESOLUTION_X, SCREEN_RESOLUTION_Y )
-  
+
   -- Make the y axis increase as you go down by
   -- scaling it by -1 (inverting it)
   viewport:setScale ( SCREEN_RESOLUTION_X, -SCREEN_RESOLUTION_Y )
-  
+
   -- Use offset projection to move the
   -- center from the middle of the screen
   -- to the top right.
@@ -31,15 +31,18 @@ function HUD:initialize ()
   -- to the viewport.
   self.layer = MOAILayer2D.new ()
   self.layer:setViewport ( self.viewport )
-  
+  partition = MOAIPartition.new()
   -- Now we need to render the layer.
   local renderTable = MOAIRenderMgr.getRenderTable ()
   table.insert ( renderTable, self.layer )
   MOAIRenderMgr.setRenderTable ( renderTable )
-  
+
   -- Add left and right indicator
   self:initializeDebugHud ()
-  
+
+  -- Add controls
+  self:initializeControls()
+  layer:setPartition(partition)
 end
 
 ------------------------------------------------
@@ -47,21 +50,25 @@ end
 -- initializes the debug notifications.
 ------------------------------------------------
 function HUD:initializeDebugHud ()
-  
+
   -- Load font using the resource manager
   self.font = MOAIFont.new ()
   self.font = ResourceManager:get ( "hudFont" )
-  
+
   -- We'll use leftRightIndicator textbox
   -- in order to display the direction 
   -- the character is facing.
   self.leftRightIndicator = self:newDebugTextBox ( 30, {10, 10, 100, 50} )
-  
+
   -- Position indicator will show
   -- the character's current position 
   -- in world coordinates.
   self.positionIndicator = self:newDebugTextBox ( 30, {10, 50, 200, 100} )
 
+end
+function HUD:initializeControls()
+  local leftButton = self:makeButton('left',60 ,SCREEN_RESOLUTION_Y - 75, 'left')
+  local rightButton = self:makeButton('right',170 ,SCREEN_RESOLUTION_Y - 75, 'right')
 end
 
 ------------------------------------------------
@@ -81,23 +88,23 @@ function HUD:newDebugTextBox ( size, rectangle )
   -- but we could have a parameter
   -- if needed
   textBox:setFont ( self.font )
-  
+
   -- we set the size of the font
   -- using the 'size' parameter
   textBox:setTextSize ( size )
-  
+
   -- we transform the rectangle
   -- table in the necessary
   -- parameters to setRect
   textBox:setRect ( unpack ( rectangle ) )
-  
+
   -- We add the textbox to
   -- the HUD's layer
   layer:insertProp ( textBox )
-
+  partition:insertProp(textBox)
   -- And finally return it
   return textBox
-  
+
 end
 
 ------------------------------------------------
@@ -107,12 +114,12 @@ end
 -- inside Game:initialize
 ------------------------------------------------
 function HUD:update ()
-  
+
   -- To update the left/right
   -- indicator, we get the scale
   -- of the character's prop.
   local x, y = Character.prop:getScl ()
-  
+
   -- If the x scale is > 0 we know it's
   -- facing left, and if it's < 0, it's 
   -- inverted, so it has to be facing
@@ -122,91 +129,54 @@ function HUD:update ()
   else
     self.leftRightIndicator:setString ( "Right" )
   end
-  
+
   -- To update the character position
   -- we need to query its Box2D body.
   -- We get its position and create
   -- a string in the format of (x,y).
   x, y = Character.physics.body:getPosition ()
   self.positionIndicator:setString ( "( " .. 
-      math.floor ( x ) .. " , " .. 
-      math.floor ( y ) .. " )" )
+    math.floor ( x ) .. " , " .. 
+    math.floor ( y ) .. " )" )
 
 end
 
 
+function HUD:makeButton (name, xloc, yloc, text)
+  local buttonGFX =ResourceManager:get('button_normal_center')
+  local  button = MOAIProp2D.new()
+  button:setDeck (buttonGFX)
+  button:setLoc (xloc,yloc)
+  button.name = name
+  layer:insertProp (button)
+  buttontext = self:addTextbox ( 0, 200, MOAITextBox.CENTER_JUSTIFY, true, text )
+  partition:insertProp(button)
 
+  return button
 
+end
 
+function HUD:addTextbox ( top, height, alignment, yflip, textinput)
+  textbox = MOAITextBox.new ()
+  textbox:setString ( textinput )
+  textbox:setFont ( font )
+  textbox:setTextSize ( 12, 326 )
+  textbox:setRect ( -280, top - height, 280, top )
+  textbox:setAlignment ( alignment )
+  textbox:setYFlip ( true )
+  layer:insertProp ( textbox )
+  return textbox
+end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---  -- setup
---  self:setupMousePosition ()
---  self:setupMemory ()
---  
---end
---
---
---function setupMousePosition ( self )
---  mouseWorldPositionTextbox:setFont ( font )
---  mouseWorldPositionTextbox:setTextSize ( 12 )
---  mouseWorldPositionTextbox:setYFlip( true )
---  mouseWorldPositionTextbox:setRect ( - SCREEN_RESOLUTION_X / 2 + 5 , SCREEN_RESOLUTION_Y / 2 + 10, - SCREEN_RESOLUTION_X / 2 + 120, SCREEN_RESOLUTION_Y / 2 + 10 - 14)
---  layer:insertProp ( mouseWorldPositionTextbox )
---  
---  mouseWindowPositionTextbox:setFont ( font )
---  mouseWindowPositionTextbox:setTextSize ( 12 )
---  mouseWindowPositionTextbox:setYFlip( true )
---  mouseWindowPositionTextbox:setRect ( - SCREEN_RESOLUTION_X / 2 + 5 , SCREEN_RESOLUTION_Y / 2 - 10, - SCREEN_RESOLUTION_X / 2 + 120, SCREEN_RESOLUTION_Y / 2 - 10 - 14)
---  layer:insertProp ( mouseWindowPositionTextbox )
---end
---
---function setupMemory( self )
---  memoryTextbox:setFont ( font )
---  memoryTextbox:setTextSize ( 12 )
---  memoryTextbox:setYFlip( true )
---  memoryTextbox:setRect ( - SCREEN_RESOLUTION_X / 2 + 5 , SCREEN_RESOLUTION_Y / 2 - 30, - SCREEN_RESOLUTION_X / 2 + 120, SCREEN_RESOLUTION_Y / 2 - 30 - 14)
---  layer:insertProp ( memoryTextbox )
---
---  self.memoryTimer = MOAITimer.new ()
---  self.memoryTimer:setSpan (0.01)
---  self.memoryTimer:setMode (MOAITimer.LOOP)
---  self.memoryTimer:setListener ( MOAITimer.EVENT_TIMER_LOOP, displayMemory)
---  self.memoryTimer:start ()
---end
---
---
---function setMouseWorldPosition(self, x,y)
---  mouseWorldPositionTextbox:setString ( "World: (" .. math.floor(x) .. "," .. math.floor(y) .. ")" )
---end
---
---function setMouseWindowPosition(self, x,y)
---  mouseWindowPositionTextbox:setString ( "Window: (" .. x .. "," .. y .. ")" )
---end
---
---function displayMemory ( timer, n)
---  memoryTextbox:setString ("Memory: " .. checkMem( true ) .. " MB ") 
---end
---function getLayer ()
---  return layer
---end
---
+function HUD:handleClickOrTouch(x, y, down)
+  local pickedProp = partition:propForPoint(layer:wndToWorld(x,y))
+  if pickedProp then
+    if pickedProp.name == 'left' then
+      Game:keyPressed ( 'left', down )
+    elseif pickedProp.name == 'right' then
+      Game:keyPressed ('right', down)
+    end
+  else
+    Game:keyPressed ('up', down)
+  end
+end
